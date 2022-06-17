d0cfeab300703862e4c80659dd2b4b946f94e7fd
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-17 22:40:02 +0900

    Add registerIncome method and AccountBookServiceTest.java #8

--- src/main/java/com/example/okozukai/service/AccountBookService.java
+++ src/main/java/com/example/okozukai/service/AccountBookService.java
@@ -1,10 +1,21 @@
 package com.example.okozukai.service;
 
+import com.example.okozukai.entity.Account;
+import com.example.okozukai.form.AccountBookForm;
+import com.example.okozukai.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountBookService {
 
+    @Autowired
+    AccountRepository accountRepository;
 
+    public void registerIncome(AccountBookForm accountBookForm) {
 
+        var account = new Account();
+
+        accountRepository.save(account);
+    }
 }
---
+++ src/test/java/com/example/okozukai/service/AccountBookServiceTest.java
@@ -0,0 +1,65 @@
+package com.example.okozukai.service;
+
+import com.example.okozukai.form.AccountBookForm;
+import com.example.okozukai.repository.AccountRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.sql.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Sql("/test-schema.sql")
+class AccountBookServiceTest {
+
+    @Autowired
+    AccountBookService accountBookService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    @DisplayName("支出が0の時、与えられた収入データが、レコードに追加される")
+    void testRegisterIncomeWithZeroValueExpense() {
+
+        var original = accountRepository.findAll();
+        assertEquals(3, original.size(), "レコード追加前のDBに保存されているデータ数の確認");
+
+
+        var accountBookForm = new AccountBookForm();
+        accountBookForm.setItemDate(Date.valueOf("2022-03-01"));
+        accountBookForm.setItem("testItem");
+        accountBookForm.setIncome(1000);
+        accountBookForm.setNote("testNote");
+
+        accountBookService.registerIncome(accountBookForm);
+
+        var actual = accountRepository.findAll();
+        assertEquals(4, actual.size(), "レコード追加後のDBに保存されているデータ数の確認");
+    }
+    @Test
+    @DisplayName("支出が0以外の時、与えられた収入データが、レコードに追加される")
+    void testRegisterIncomeWithNonZeroValueExpense() {
+
+        var original = accountRepository.findAll();
+        assertEquals(3, original.size(), "レコード追加前のDBに保存されているデータ数の確認");
+
+
+        var accountBookForm = new AccountBookForm();
+        accountBookForm.setItemDate(Date.valueOf("2022-03-01"));
+        accountBookForm.setItem("testItem");
+        accountBookForm.setIncome(1000);
+        accountBookForm.setExpense(2000);
+        accountBookForm.setNote("testNote");
+
+        accountBookService.registerIncome(accountBookForm);
+
+        var actual = accountRepository.findAll();
+        assertEquals(4, actual.size(), "レコード追加後のDBに保存されているデータ数の確認");
+    }
+
+}
