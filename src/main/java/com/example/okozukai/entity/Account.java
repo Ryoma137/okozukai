7569df753283258b35cc48ad3aa6098abca0939a
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-17 16:14:56 +0900

    Fix test-schema.sql and test-schema-not-data-exist.sql and some tests in AccountRepositoryTest

--- src/main/java/com/example/okozukai/entity/Account.java
+++ src/main/java/com/example/okozukai/entity/Account.java
@@ -17,7 +17,7 @@ import java.sql.Date;
 public class Account {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Date itemDate;
