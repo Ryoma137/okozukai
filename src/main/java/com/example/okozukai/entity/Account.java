8f72ace1a5352ec07c9743f48b4beaae23fae324
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-17 12:10:02 +0900

    Add some variables, Entity and Dara annotation #2

--- src/main/java/com/example/okozukai/entity/Account.java
+++ src/main/java/com/example/okozukai/entity/Account.java
@@ -1,4 +1,34 @@
 package com.example.okozukai.entity;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Timer;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Date itemDate;
+
+    private String item;
+
+    private int income;
+
+    private int expense;
+
+    private String note;
+
 }
