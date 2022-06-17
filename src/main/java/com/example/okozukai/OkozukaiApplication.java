9f6789772b2086e33bb2c4686b9647af0a9deabe
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-17 10:47:43 +0900

    initial commit

---
+++ src/main/java/com/example/okozukai/OkozukaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.okozukai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkozukaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkozukaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/okozukai/OkozukaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okozukai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkozukaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
