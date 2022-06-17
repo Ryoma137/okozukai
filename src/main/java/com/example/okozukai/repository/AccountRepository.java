76540fcf60d6b3caa0870363fb119e1253bf7b2e
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-17 20:07:28 +0900

    Create AccountBookService

--- src/main/java/com/example/okozukai/repository/AccountRepository.java
+++ src/main/java/com/example/okozukai/repository/AccountRepository.java
@@ -2,7 +2,9 @@ package com.example.okozukai.repository;
 
 import com.example.okozukai.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
 
 
