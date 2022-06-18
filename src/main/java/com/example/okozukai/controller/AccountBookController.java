b78e86410602b649f7cf0e554b177c1e0d7ab67c
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-18 10:24:10 +0900

    Create AccountBookController #10

---
+++ src/main/java/com/example/okozukai/controller/AccountBookController.java
@@ -0,0 +1,33 @@
+package com.example.okozukai.controller;
+
+
+import com.example.okozukai.form.AccountBookForm;
+import com.example.okozukai.service.AccountBookService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@AllArgsConstructor
+public class AccountBookController {
+
+    private final AccountBookService accountBookService;
+
+    @GetMapping("/account-book/new")
+    public String getRegisterPage() {
+
+        return "account-book/new";
+    }
+
+
+    @PostMapping("/account-book/new")
+    public String registerIncome(AccountBookForm accountBookForm) {
+
+        accountBookService.registerIncome(accountBookForm);
+        return "account-book/new";
+
+    }
+
+
+}
