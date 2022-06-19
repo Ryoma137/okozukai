afd5e0018f1420ccf2837ef6046e214e28a471bd
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-19 18:00:03 +0900

    (WIP)

--- src/main/java/com/example/okozukai/controller/AccountBookController.java
+++ src/main/java/com/example/okozukai/controller/AccountBookController.java
@@ -6,6 +6,7 @@ import com.example.okozukai.service.AccountBookService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -15,14 +16,13 @@ public class AccountBookController {
     private final AccountBookService accountBookService;
 
     @GetMapping("/account-book/new")
-    public String getRegisterPage() {
+    public String getRegisterPage(@ModelAttribute("registerInfo") AccountBookForm accountBookForm) {
 
-        return "account-book/new";
+        return "new";
     }
 
-
     @PostMapping("/account-book/new")
-    public String registerIncome(AccountBookForm accountBookForm) {
+    public String registerIncome(@ModelAttribute("registerInfo") AccountBookForm accountBookForm) {
 
         accountBookService.registerIncome(accountBookForm);
         return "account-book/new";
--- src/main/java/com/example/okozukai/service/AccountBookService.java
+++ src/main/java/com/example/okozukai/service/AccountBookService.java
@@ -4,18 +4,25 @@ import com.example.okozukai.entity.Account;
 import com.example.okozukai.form.AccountBookForm;
 import com.example.okozukai.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountBookService {
 
+    private final JdbcTemplate jdbcTemplate;
+
     @Autowired
     AccountRepository accountRepository;
+
+    public AccountBookService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public void registerIncome(AccountBookForm accountBookForm) {
 
         var account = new Account();
-
+        account.setId(null);
         accountRepository.save(account);
     }
 }
