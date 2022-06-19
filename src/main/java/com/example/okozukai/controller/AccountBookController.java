517543e6457328629da9384196d0a86f1e1b549f
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-19 22:57:05 +0900

    Fix some thymeleaf codes and registerIncome in AccountBookController in order to redirect properly　#12

--- src/main/java/com/example/okozukai/controller/AccountBookController.java
+++ src/main/java/com/example/okozukai/controller/AccountBookController.java
@@ -25,7 +25,7 @@ public class AccountBookController {
     public String registerIncome(@ModelAttribute("registerInfo") AccountBookForm accountBookForm) {
 
         accountBookService.registerIncome(accountBookForm);
-        return "new";
+        return "/new";
 
     }
 
