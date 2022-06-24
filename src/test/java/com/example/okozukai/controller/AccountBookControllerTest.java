4b67db5b81335ea6bb73c28f9b0a851ded2239ca
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-24 13:29:12 +0900

    Fix testCallRegisterIncomeFunctionWhenSelectedIncome and testCallRegisterExpenseFunctionWhenSelectedExpense  #23

--- src/test/java/com/example/okozukai/controller/AccountBookControllerTest.java
+++ src/test/java/com/example/okozukai/controller/AccountBookControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.okozukai.controller;
 
 import com.example.okozukai.form.AccountBookForm;
+import com.example.okozukai.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.sql.Date;
 
@@ -17,8 +19,12 @@ class AccountBookControllerTest {
     @Autowired
     AccountBookController accountBookController;
 
+    @Autowired
+    AccountRepository accountRepository;
+
     @Test
-    @DisplayName("radio buttonで支出を選択している時、収入情報を登録するメソッドが呼ばれる")
+    @Sql("/test-schema-not-data-exist.sql")
+    @DisplayName("PriceTypeの値がincomeの時、収入情報を登録するメソッドが呼ばれる")
     void testCallRegisterIncomeFunctionWhenSelectedIncome() {
 
         var accountBookForm = new AccountBookForm();
@@ -30,13 +36,17 @@ class AccountBookControllerTest {
 
         accountBookController.registerInfo(accountBookForm);
 
-        assertEquals("income", accountBookForm.getPriceType(), "priceRadioの値がincomeであること確認");
+        var actual = accountRepository.findAll();
+
+        assertEquals(1000, actual.get(0).getIncome(), "収入情報を登録するメソッドが呼ばれ、収入情報が登録されていることを確認");
+        assertEquals(0, actual.get(0).getExpense(), "収入情報を登録するメソッドが呼ばれるため、支出情報が登録されていないことを確認");
 
     }
 
     @Test
-    @DisplayName("radio buttonで支出を選択している時、支出情報を登録するメソッドが呼ばれる")
-    void testCallRegisterExpenseFunctionWhenSelectedIncome() {
+    @Sql("/test-schema-not-data-exist.sql")
+    @DisplayName("PriceTypeの値がexpenseの時、支出情報を登録するメソッドが呼ばれる")
+    void testCallRegisterExpenseFunctionWhenSelectedExpense() {
 
         var accountBookForm = new AccountBookForm();
         accountBookForm.setPriceType("expense");
@@ -46,8 +56,10 @@ class AccountBookControllerTest {
         accountBookForm.setNote("testNote");
 
         accountBookController.registerInfo(accountBookForm);
+        var actual = accountRepository.findAll();
 
-        assertEquals("expense", accountBookForm.getPriceType(), "priceRadioの値がincomeであること確認");
+        assertEquals(1000, actual.get(0).getExpense(), "支出情報を登録するメソッドが呼ばれ、支出情報が登録されていることを確認");
+        assertEquals(0, actual.get(0).getIncome(), "支出情報を登録するメソッドが呼ばれるため、収入情報が登録されていないことを確認");
 
     }
 }
