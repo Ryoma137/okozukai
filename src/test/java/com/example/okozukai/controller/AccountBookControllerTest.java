f3c6463fb618d78a851a047c2e4f0ccc8741855c
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-24 09:47:56 +0900

    Change variable name from priceRadio to priceType

--- src/test/java/com/example/okozukai/controller/AccountBookControllerTest.java
+++ src/test/java/com/example/okozukai/controller/AccountBookControllerTest.java
@@ -4,15 +4,12 @@ import com.example.okozukai.form.AccountBookForm;
 import com.example.okozukai.service.AccountBookService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.security.auth.login.AccountException;
 import java.sql.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -21,15 +18,12 @@ class AccountBookControllerTest {
     @Autowired
     AccountBookController accountBookController;
 
-    @Autowired
-    AccountBookService accountBookService;
-
     @Test
     @DisplayName("radio buttonで支出を選択している時、収入情報を登録するメソッドが呼ばれる")
-    void testCallRegisterIncomeFunctionWhenSelectedIncome() {
+    void testCallRegisterIncomeMethodWhenSelectedIncome() {
 
         var accountBookForm = new AccountBookForm();
-        accountBookForm.setPriceRadio("income");
+        accountBookForm.setPriceType("income");
         accountBookForm.setItemDate(Date.valueOf("2022-03-01"));
         accountBookForm.setItem("testItem");
         accountBookForm.setPrice(1000);
@@ -37,16 +31,16 @@ class AccountBookControllerTest {
 
         accountBookController.registerInfo(accountBookForm);
 
-        assertEquals("income", accountBookForm.getPriceRadio(), "priceRadioの値がincomeであること確認");
+        assertEquals("income", accountBookForm.getPriceType(), "priceRadioの値がincomeであること確認");
 
     }
 
     @Test
     @DisplayName("radio buttonで支出を選択している時、支出情報を登録するメソッドが呼ばれる")
-    void testCallRegisterExpenseFunctionWhenSelectedIncome() {
+    void testCallRegisterExpenseMethodWhenSelectedIncome() {
 
         var accountBookForm = new AccountBookForm();
-        accountBookForm.setPriceRadio("expense");
+        accountBookForm.setPriceType("expense");
         accountBookForm.setItemDate(Date.valueOf("2022-03-01"));
         accountBookForm.setItem("testItem");
         accountBookForm.setPrice(1000);
@@ -54,7 +48,7 @@ class AccountBookControllerTest {
 
         accountBookController.registerInfo(accountBookForm);
 
-        assertEquals("expense", accountBookForm.getPriceRadio(), "priceRadioの値がincomeであること確認");
+        assertEquals("expense", accountBookForm.getPriceType(), "priceTypeの値がincomeであること確認");
 
     }
 }
